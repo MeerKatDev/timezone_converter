2943a499f81f3ae91b9950d0f492380e88ea66a4
Luca <lcampobasso@gmail.com>
2022-02-04 11:07:11 +0100

    removed some boilerplate tests and the vanilla js version

--- lib/timezone_converter_web/controllers/page_controller.ex
+++ lib/timezone_converter_web/controllers/page_controller.ex
@@ -1,8 +1,7 @@
 defmodule TimezoneConverterWeb.PageController do
   use TimezoneConverterWeb, :controller
-  alias TimezoneConverter.Models.TimeZone, as: TZ
 
   def index(conn, _params) do
-    render(conn, "index.html", timezones: TZ.all())
+    render(conn, "index.html")
   end
 end
--- test/timezone_converter_web/live/time_zone_live_test.exs
+++ test/timezone_converter_web/live/time_zone_live_test.exs
@@ -2,11 +2,6 @@ defmodule TimezoneConverterWeb.TimeZoneLiveTest do
   use TimezoneConverterWeb.ConnCase
 
   import Phoenix.LiveViewTest
-  import TimezoneConverter.ModelsFixtures
-
-  @create_attrs %{name: "some name"}
-  @update_attrs %{name: "some updated name"}
-  @invalid_attrs %{name: nil}
 
   defp create_time_zone(_) do
     time_zone = time_zone_fixture()
@@ -23,7 +18,6 @@ defmodule TimezoneConverterWeb.TimeZoneLiveTest do
       assert html =~ time_zone.name
     end
 
-    test "saves new time_zone", %{conn: conn} do
       {:ok, index_live, _html} = live(conn, Routes.time_zone_index_path(conn, :index))
 
       assert index_live |> element("a", "New Time zone") |> render_click() =~
@@ -44,67 +38,4 @@ defmodule TimezoneConverterWeb.TimeZoneLiveTest do
       assert html =~ "Time zone created successfully"
       assert html =~ "some name"
     end
-
-    test "updates time_zone in listing", %{conn: conn, time_zone: time_zone} do
-      {:ok, index_live, _html} = live(conn, Routes.time_zone_index_path(conn, :index))
-
-      assert index_live |> element("#time_zone-#{time_zone.id} a", "Edit") |> render_click() =~
-               "Edit Time zone"
-
-      assert_patch(index_live, Routes.time_zone_index_path(conn, :edit, time_zone))
-
-      assert index_live
-             |> form("#time_zone-form", time_zone: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        index_live
-        |> form("#time_zone-form", time_zone: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.time_zone_index_path(conn, :index))
-
-      assert html =~ "Time zone updated successfully"
-      assert html =~ "some updated name"
-    end
-
-    test "deletes time_zone in listing", %{conn: conn, time_zone: time_zone} do
-      {:ok, index_live, _html} = live(conn, Routes.time_zone_index_path(conn, :index))
-
-      assert index_live |> element("#time_zone-#{time_zone.id} a", "Delete") |> render_click()
-      refute has_element?(index_live, "#time_zone-#{time_zone.id}")
-    end
-  end
-
-  describe "Show" do
-    setup [:create_time_zone]
-
-    test "displays time_zone", %{conn: conn, time_zone: time_zone} do
-      {:ok, _show_live, html} = live(conn, Routes.time_zone_show_path(conn, :show, time_zone))
-
-      assert html =~ "Show Time zone"
-      assert html =~ time_zone.name
-    end
-
-    test "updates time_zone within modal", %{conn: conn, time_zone: time_zone} do
-      {:ok, show_live, _html} = live(conn, Routes.time_zone_show_path(conn, :show, time_zone))
-
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Time zone"
-
-      assert_patch(show_live, Routes.time_zone_show_path(conn, :edit, time_zone))
-
-      assert show_live
-             |> form("#time_zone-form", time_zone: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      {:ok, _, html} =
-        show_live
-        |> form("#time_zone-form", time_zone: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.time_zone_show_path(conn, :show, time_zone))
-
-      assert html =~ "Time zone updated successfully"
-      assert html =~ "some updated name"
-    end
-  end
 end
